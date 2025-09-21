bd535f75d3cad769e4a5236bc05b919caf4100b5
Moritz344 <moritzmaier34@gmail.com>
2025-09-21 14:31:14 +0200

    fix

--- bash_scripts/random_wallpaper.sh
+++ bash_scripts/random_wallpaper.sh
@@ -1,5 +1,15 @@
 #!/bin/bash 
 
+<<<<<<< HEAD
 ls /home/moritz/Bilder/wallpapers |sort -R |tail -1 |while read file; do
     feh --bg-scale /home/moritz/Bilder/wallpapers/$file /home/moritz/Bilder/wallpapers/$file
 done
+=======
+
+ls /home/moritz/Bilder/wallpapers |sort -R |tail -1 |while read file; do
+    feh --bg-scale /home/moritz/Bilder/wallpapers/$file /home/moritz/Bilder/wallpapers/$file
+done
+
+
+
+>>>>>>> 998f34f (better)
