37e95b60a95f531969bdeb246c4e06d3f101ff6b
Moritz344 <moritzmaier34@gmail.com>
2026-04-24 13:26:09 +0200

    removed stuff

--- scripts/bash_scripts/minecraft.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
- env __GL_THREADED_OPTIMIZATIONS=0 __GLX_VENDOR_LIBRARY_NAME=nvidia /home/moritz/Downloads/lunar-client.AppImage --no-sandbox
-
--- scripts/bash_scripts/random_wallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BACKGROUNDS=(/home/moritz/.config/omarchy/current/theme/backgrounds/*)
-RANDOM_BG=${BACKGROUNDS[$RANDOM % ${#BACKGROUNDS[@]}]}
-
-uwsm-app -- swaybg -i "$RANDOM_BG" -m fill
