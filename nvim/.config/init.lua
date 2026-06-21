d413c402da077e5c4cb5f535a76f3d3b839b017f
Moritz344 <moritzmaier34@gmail.com>
2026-06-21 13:46:00 +0200

    add plugins/keybindings/

--- nvim/.config/init.lua
+++ nvim/.config/init.lua
@@ -1,8 +1,8 @@
 
--- https://neovim.io/doc/user/lsp/
-
 vim.pack.add({
   { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = "https://github.com/nvim-telescope/telescope.nvim"},
+  { src = "https://github.com/nvim-lua/plenary.nvim"},
   { src = 'https://github.com/stevearc/oil.nvim' },
   { src = 'https://github.com/yonatanperel/lake-dweller.nvim' },
   { src = 'https://github.com/vague-theme/vague.nvim' },
@@ -12,6 +12,11 @@ vim.pack.add({
   { src = 'https://github.com/lewis6991/gitsigns.nvim' },
   { src = 'https://github.com/windwp/nvim-ts-autotag' },
   { src = 'https://github.com/echasnovski/mini.nvim' },
+  { src = 'https://github.com/neovim-treesitter/nvim-treesitter' },
+  { src = 'https://github.com/neovim-treesitter/treesitter-parser-registry'},
+  { src = 'https://github.com/AlexvZyl/nordic.nvim' },
+  { src = 'https://github.com/IogaMaster/neocord' },
+  { src = 'https://github.com/fenetikm/falcon' },
 })
 
 require('mini.notify').setup()
@@ -47,12 +52,41 @@ require('mini.starter').setup({
 
 })
 
+
 vim.diagnostic.config({
   ["my/notify"] = {
     log_level = vim.log.levels.INFO,
     severity = vim.diagnostic.severity.ERROR,
   }
 })
+
+require("neocord").setup({
+    -- General options
+    logo                = "auto",                     -- "auto" or url
+    logo_tooltip        = nil,                        -- nil or string
+    main_image          = "language",                 -- "language" or "logo"
+    client_id           = "1157438221865717891",      -- Use your own Discord application client id (not recommended)
+    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+    file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+    show_time           = true,                       -- Show the timer
+    global_timer        = false,                      -- if set true, timer won't update when any event are triggered
+    buttons             = nil,                        -- A list of buttons (objects with label and url attributes) or a function returning such list.
+
+    -- Rich Presence text options
+    editing_text           = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text     = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text        = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text    = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    reading_text           = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    workspace_text         = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    line_number_text       = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+    terminal_text          = "Using Terminal",           -- Format string rendered when in terminal mode.
+    task_management_text   = "Managing Tasks"            -- Format string rendered when editing a todo.txt, done.txt or using tuxedo.nvim.
+})
+
+
 
 
 require("nvim-ts-autotag").setup({
@@ -69,44 +103,32 @@ require("nvim-ts-autotag").setup({
 });
 require('lspconfig').ts_ls.setup({})
 require('lspconfig').angularls.setup({})
+require('lspconfig').intelephense.setup({})
+require('lspconfig').clangd.setup({})
 require("oil").setup()
 require("lake-dweller").setup()
-require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-    json = { "prettier" },
-    css = { "prettier" },
-    html = { "prettier" },
-  },
-  format_on_save = {
-    timeout_ms = 500,
-    pattern = "*",
-  },
-})
 require("gitsigns").setup({})
 
-vim.lsp.enable({ 'ts_ls', 'angularls' })
+vim.lsp.enable({ 'ts_ls', 'angularls', 'intelephense', 'clangd' })
 
 
 -- General Settings
 vim.opt.completeopt = { 'menuone', 'noselect', 'popup' }
 vim.opt.number = true
 vim.opt.expandtab = true
+vim.o.guifont = "JetBrainsMono Nerd Font:h10"
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-vim.cmd.colorscheme("vague")
 vim.opt.relativenumber = true
 vim.opt.cursorline = false
 vim.opt.swapfile = false
 vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block'
 vim.g.mapleader = " "
 
+
+vim.cmd.colorscheme("vague")
 
 -- Keybinds
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
@@ -118,6 +140,21 @@ vim.lsp.handlers.hover = function(_, result, ctx, config)
   return vim.lsp._meta.protocol.handlers["textDocument/hover"](_, result, ctx, config)
 end
 
+if vim.g.neovide then
+  vim.keymap.set({"n", "v"}, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  vim.keymap.set({"n", "v"}, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+  vim.keymap.set({"n", "v"}, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+end
+
+vim.keymap.set("n", "<leader>th", function()
+  require("telescope.builtin").colorscheme()
+end, { desc = "Colorscheme picker" })
+
+
+vim.keymap.set("n", "<C-Tab>", "gt", { desc = "Next tab" })
+vim.keymap.set("n", "<C-S-Tab>", "gT", { desc = "Prev tab" })
+
+
 vim.api.nvim_create_autocmd("CursorHold", {
   callback = function()
     vim.diagnostic.open_float(nil, { focus = false })
@@ -125,7 +162,6 @@ vim.api.nvim_create_autocmd("CursorHold", {
 })
 
 
+
 vim.keymap.set("n", "K", vim.diagnostic.open_float, { desc = "Show diagnostic" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Quickfix diagnostics" })
-vim.keymap.set("n", "<leader>l", ":copen<CR>", { desc = "Open quickfix" })
-
