c483b21e64c559b7b27b50168073685e0696fc37
Moritz344 <moritzmaier34@gmail.com>
2026-08-03 11:38:53 +0200

    remove unused scripts

--- scripts/bash_scripts/launch_nero_floating.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uwsm-app -- xdg-terminal-exec -e /home/moritz/programming/bash_scripts/start_nero_launcher.sh
--- scripts/bash_scripts/run_paint_app.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-SDL_VIDEODRIVER=x11 flatpak run com.infinipaint.infinipaint
